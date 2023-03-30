45b8f5bc31750df6753f0a01bed9482c19013431
Roshan Jha <Roshan Jha@SMTC-R-0204>
2023-03-30 16:37:38 +0530

    first python code

--- intro/selenium_automation.py
+++ intro/selenium_automation.py
@@ -1,1 +1,11 @@
 print("Hello world of selenium")
+import time
+from selenium  import webdriver
+
+driver =webdriver.Chrome()
+driver.get("https://www.ecoders.in")
+time.sleep(5)
+print(driver.title)
+print(driver.current_url)
+print(driver.page_source)
+driver.quit()
